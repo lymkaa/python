3d6fc1a6a53d11c47fb648b13f59fc0daa63f076
lymkaa <kalmagambetova.arai@icloud.com>
2026-04-18 01:56:20 +0500

    *commit practice9

--- practice 9/moving_ball/ball.py
+++ practice 9/moving_ball/ball.py
@@ -1,38 +1,36 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((1200, 700))   #size
-WHITE = (255, 255, 255) #
-RED = (255, 0, 0) # code numbers
+
+screen = pygame.display.set_mode((1200, 700))  # Размер окна
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
 done = False
-
-
-
 clock = pygame.time.Clock()
 
 circle_start_w = 600
 circle_start_h = 350
 
 while not done:
-    
     keys = pygame.key.get_pressed()
-    pygame.draw.circle(screen, RED, (circle_start_w, circle_start_h), 25) # rad 25, red, we drew
+    screen.fill(WHITE)  
+
+    if keys[pygame.K_UP] and circle_start_h > 38:
+        circle_start_h -= 20
+    if keys[pygame.K_DOWN] and circle_start_h < 662:
+        circle_start_h += 20
+    if keys[pygame.K_LEFT] and circle_start_w > 38:
+        circle_start_w -= 20
+    if keys[pygame.K_RIGHT] and circle_start_w < 1162:
+        circle_start_w += 20
+
+    pygame.draw.circle(screen, RED, (circle_start_w, circle_start_h), 25) 
+    pygame.display.flip() 
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    if keys[pygame.K_UP]:
-        if (circle_start_h > 38):   
-            circle_start_h -= 20
-    if keys[pygame.K_DOWN]:
-        if (circle_start_h < 662):
-            circle_start_h += 20    #h movement
-    if keys[pygame.K_LEFT]:
-        if (circle_start_w > 38):
-            circle_start_w -= 20
-    if keys[pygame.K_RIGHT]:
-        if (circle_start_w < 1162):
-            circle_start_w += 20    #w movement
-    
-    screen.fill(WHITE)
-    pygame.draw.circle(screen, RED, (circle_start_w, circle_start_h), 25)
-    pygame.display.flip()
+
     clock.tick(60) 
+
+pygame.quit()
